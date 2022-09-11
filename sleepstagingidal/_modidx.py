65be18f406ceadd1beee0db0c1fe36f93279139f
Jorgvt <vila_tronik@hotmail.com>
2022-09-12 00:05:11 +0200

    :ambulance: nbdev has problems with more than one notebook sharing default_exp, so had to change it in sanity checking to build the docs.

--- sleepstagingidal/_modidx.py
+++ sleepstagingidal/_modidx.py
@@ -5,4 +5,10 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://Jorgvt.github.io',
                 'git_url': 'https://github.com/Jorgvt/sleepstagingidal',
                 'lib_path': 'sleepstagingidal'},
-  'syms': {'sleepstagingidal.data': {'sleepstagingidal.data.get_epochs': ('data_loading.html#get_epochs', 'sleepstagingidal/data.py')}}}
+  'syms': { 'sleepstagingidal.data': {'sleepstagingidal.data.get_epochs': ('data_loading.html#get_epochs', 'sleepstagingidal/data.py')},
+            'sleepstagingidal.dataa': { 'sleepstagingidal.dataa.get_sleep_stages': ( 'sanity_checking.html#get_sleep_stages',
+                                                                                     'sleepstagingidal/dataa.py'),
+                                        'sleepstagingidal.dataa.map_events': ( 'sanity_checking.html#map_events',
+                                                                               'sleepstagingidal/dataa.py'),
+                                        'sleepstagingidal.dataa.swap_dict': ( 'sanity_checking.html#swap_dict',
+                                                                              'sleepstagingidal/dataa.py')}}}
